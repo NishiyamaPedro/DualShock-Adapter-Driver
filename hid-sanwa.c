3343f5ec3d3e46665af787b7b0cea9db593443d5
Pedro Nishiyama <nishiyama.v3@gmail.com>
2021-12-23 03:54:00 -0300

    inputs from both ports have been implemented

--- hid-sanwa.c
+++ hid-sanwa.c
@@ -1,13 +1,30 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 /*
  * Copyright (c) 2021 Pedro Nishiyama <nishiyama.v3@gmail.com>
- * Based on hid-dualsense driver
+ * Based on hid-playstation driver
+ * 
+ * TO-DO
+ * - Implement Force Feedback
+ * 
  */
 
 #include <linux/input.h>
-#include <linux/slab.h>
 #include <linux/hid.h>
 #include <linux/module.h>
+
+#define BUTTONS0_HAT_SWITCH	GENMASK(3, 0)
+#define BUTTONS0_CROSS		BIT(4)
+#define BUTTONS0_CIRCLE		BIT(5)
+#define BUTTONS0_SQUARE		BIT(6)
+#define BUTTONS0_TRIANGLE	BIT(7)
+#define BUTTONS1_L1			BIT(0)
+#define BUTTONS1_L2			BIT(1)
+#define BUTTONS1_R1			BIT(2)
+#define BUTTONS1_R2			BIT(3)
+#define BUTTONS1_SELECT		BIT(4)
+#define BUTTONS1_START		BIT(5)
+#define BUTTONS1_L3			BIT(6)
+#define BUTTONS1_R3			BIT(7)
 
 static const int gamepad_buttons[] = {
 	BTN_WEST,
@@ -22,6 +39,22 @@ static const int gamepad_buttons[] = {
 	BTN_START,
 	BTN_THUMBL,
 	BTN_THUMBR,
+};
+
+struct sanwa_adapter {
+	struct input_dev *port_one;
+	struct input_dev *port_two;	
+};
+
+struct sanwa_input_report {
+	uint8_t rz, z; // Right Y and X axis
+	uint8_t x, y; // Left X and Y axis
+	uint8_t buttons[3]; 
+};
+
+static const struct {int x; int y; } sanwa_gamepad_hat_mapping[] = {
+	{0, -1}, {1, -1}, {1, 0}, {1, 1}, {0, 1}, {-1, 1}, {-1, 0}, {-1, -1},
+	{0, 0},
 };
 
 static struct input_dev *sanwa_allocate_input_dev(struct hid_device *hdev, const char *sfx)
@@ -53,8 +86,6 @@ static void sanwa_set_capabilities(struct input_dev *port)
 	input_set_abs_params(port, ABS_X, 0, 255, 0, 0);
 	input_set_abs_params(port, ABS_Y, 0, 255, 0, 0);
 	input_set_abs_params(port, ABS_Z, 0, 255, 0, 0);
-	input_set_abs_params(port, ABS_RX, 0, 255, 0, 0);
-	input_set_abs_params(port, ABS_RY, 0, 255, 0, 0);
 	input_set_abs_params(port, ABS_RZ, 0, 255, 0, 0);
 
 	input_set_abs_params(port, ABS_HAT0X, -1, 1, 0, 0);
@@ -66,21 +97,26 @@ static void sanwa_set_capabilities(struct input_dev *port)
 
 static int sanwa_create_inputs(struct hid_device *hdev)
 {
-	struct input_dev *port_one;
-	struct input_dev *port_two;
+	struct sanwa_adapter *sa;
 	int ret;
 
+	sa = devm_kzalloc(&hdev->dev, sizeof(*sa), GFP_KERNEL);
+	if (!sa)
+		return 1;
+
+	hid_set_drvdata(hdev, sa);
+
 	hid_err(hdev, "creating port 1\n");
-	port_one = sanwa_allocate_input_dev(hdev, "Port 1");
-	sanwa_set_capabilities(port_one);
-	ret = input_register_device(port_one);
+	sa->port_one = sanwa_allocate_input_dev(hdev, "Port 1");
+	sanwa_set_capabilities(sa->port_one);
+	ret = input_register_device(sa->port_one);
 	if (ret)
 		return ret;
 
 	hid_err(hdev, "creating port 2\n");
-	port_two = sanwa_allocate_input_dev(hdev, "Port 2");
-	sanwa_set_capabilities(port_two);
-	ret = input_register_device(port_two);
+	sa->port_two = sanwa_allocate_input_dev(hdev, "Port 2");
+	sanwa_set_capabilities(sa->port_two);
+	ret = input_register_device(sa->port_two);
 	if (ret)
 		return ret;
 	
@@ -126,7 +162,48 @@ err_stop:
 
 static int sanwa_raw_event(struct hid_device *hdev, struct hid_report *report, u8 *data, int size)
 {
-	/* TO-DO */
+	struct sanwa_adapter *sa = hid_get_drvdata(hdev);
+	struct sanwa_input_report *sa_report = (struct sanwa_input_report *)&data[1];
+	struct input_dev *gamepad;
+	uint8_t value;
+	
+	if (report->id == 1)
+		hid_info(hdev, "%u", sa_report->buttons[0]);
+
+	if (report->id == 1)
+		gamepad = sa->port_one;
+	else if (report->id == 2)
+		gamepad = sa->port_two;
+
+	if (gamepad) {
+		input_report_abs(gamepad, ABS_X,  sa_report->x);
+		input_report_abs(gamepad, ABS_Y,  sa_report->y);
+		input_report_abs(gamepad, ABS_Z,  sa_report->z);
+		input_report_abs(gamepad, ABS_RZ, sa_report->rz);
+		
+		value = sa_report->buttons[0] & BUTTONS0_HAT_SWITCH;
+		if (value >= ARRAY_SIZE(sanwa_gamepad_hat_mapping))
+			value = 8;
+		input_report_abs(gamepad, ABS_HAT0X, sanwa_gamepad_hat_mapping[value].x);
+		input_report_abs(gamepad, ABS_HAT0Y, sanwa_gamepad_hat_mapping[value].y);
+
+		input_report_key(gamepad, BTN_WEST,   sa_report->buttons[0] & BUTTONS0_SQUARE);
+		input_report_key(gamepad, BTN_SOUTH,  sa_report->buttons[0] & BUTTONS0_CROSS);
+		input_report_key(gamepad, BTN_EAST,   sa_report->buttons[0] & BUTTONS0_CIRCLE);
+		input_report_key(gamepad, BTN_NORTH,  sa_report->buttons[0] & BUTTONS0_TRIANGLE);
+
+		input_report_key(gamepad, BTN_TL,     sa_report->buttons[1] & BUTTONS1_L1);
+		input_report_key(gamepad, BTN_TR,     sa_report->buttons[1] & BUTTONS1_R1);
+		input_report_key(gamepad, BTN_TL2,    sa_report->buttons[1] & BUTTONS1_L2);
+		input_report_key(gamepad, BTN_TR2,    sa_report->buttons[1] & BUTTONS1_R2);
+		input_report_key(gamepad, BTN_THUMBL, sa_report->buttons[1] & BUTTONS1_L3);
+		input_report_key(gamepad, BTN_THUMBR, sa_report->buttons[1] & BUTTONS1_R3);	
+		input_report_key(gamepad, BTN_SELECT, sa_report->buttons[1] & BUTTONS1_SELECT);
+		input_report_key(gamepad, BTN_START,  sa_report->buttons[1] & BUTTONS1_START);
+	
+		input_sync(gamepad);
+	}
+
 	return 0;
 }
 
