eecb5089ab2340405a9f643a9ff46efa8f701e51
Pedro Nishiyama <nishiyama.v3@gmail.com>
2023-07-09 05:14:36 -0300

    indent code

--- hid-sanwa.c
+++ hid-sanwa.c
@@ -2,29 +2,29 @@
 /*
  * Copyright (c) 2021 Pedro Nishiyama <nishiyama.v3@gmail.com>
  * Based on hid-playstation driver
- * 
+ *
  * TO-DO
  * - Implement Force Feedback
- * 
+ *
  */
 
 #include <linux/input.h>
 #include <linux/hid.h>
 #include <linux/module.h>
 
-#define BUTTONS0_HAT_SWITCH	GENMASK(3, 0)
-#define BUTTONS0_CROSS		BIT(4)
-#define BUTTONS0_CIRCLE		BIT(5)
-#define BUTTONS0_SQUARE		BIT(6)
-#define BUTTONS0_TRIANGLE	BIT(7)
-#define BUTTONS1_L1			BIT(0)
-#define BUTTONS1_L2			BIT(1)
-#define BUTTONS1_R1			BIT(2)
-#define BUTTONS1_R2			BIT(3)
-#define BUTTONS1_SELECT		BIT(4)
-#define BUTTONS1_START		BIT(5)
-#define BUTTONS1_L3			BIT(6)
-#define BUTTONS1_R3			BIT(7)
+#define BUTTONS0_HAT_SWITCH GENMASK(3, 0)
+#define BUTTONS0_CROSS BIT(4)
+#define BUTTONS0_CIRCLE BIT(5)
+#define BUTTONS0_SQUARE BIT(6)
+#define BUTTONS0_TRIANGLE BIT(7)
+#define BUTTONS1_L1 BIT(0)
+#define BUTTONS1_L2 BIT(1)
+#define BUTTONS1_R1 BIT(2)
+#define BUTTONS1_R2 BIT(3)
+#define BUTTONS1_SELECT BIT(4)
+#define BUTTONS1_START BIT(5)
+#define BUTTONS1_L3 BIT(6)
+#define BUTTONS1_R3 BIT(7)
 
 static const int gamepad_buttons[] = {
 	BTN_WEST,
@@ -41,15 +41,17 @@ static const int gamepad_buttons[] = {
 	BTN_THUMBR,
 };
 
-struct sanwa_adapter {
+struct sanwa_adapter
+{
 	struct input_dev *port_one;
 	struct input_dev *port_two;
 };
 
-struct sanwa_input_report {
+struct sanwa_input_report
+{
 	uint8_t ry, rx; // Right Y and X axis
-	uint8_t x, y; // Left X and Y axis
-	uint8_t buttons[3]; 
+	uint8_t x, y;	// Left X and Y axis
+	uint8_t buttons[3];
 };
 
 static const struct {int x; int y; } sanwa_gamepad_hat_mapping[] = {
@@ -69,11 +71,11 @@ static struct input_dev *sanwa_allocate_input_dev(struct hid_device *hdev, const
 	input_dev->id.vendor = hdev->vendor;
 	input_dev->id.product = hdev->product;
 	input_dev->id.version = hdev->version;
-	
+
 	input_dev->name = devm_kasprintf(&hdev->dev, GFP_KERNEL, "%s %s", hdev->name, sfx);
 	if (!input_dev->name)
 		return ERR_PTR(-ENOMEM);
-	
+
 	input_dev->phys = devm_kasprintf(&hdev->dev, GFP_KERNEL, "%s", hdev->phys);
 	if (!input_dev->phys)
 		return ERR_PTR(-ENOMEM);
@@ -118,20 +120,20 @@ static int sanwa_create_inputs(struct hid_device *hdev)
 
 	hid_set_drvdata(hdev, sa);
 
-	hid_err(hdev, "creating port 1\n");
+	hid_info(hdev, "creating port 1\n");
 	sa->port_one = sanwa_allocate_input_dev(hdev, "Port 1");
 	sanwa_set_capabilities(sa->port_one);
 	ret = input_register_device(sa->port_one);
 	if (ret)
 		return ret;
 
-	hid_err(hdev, "creating port 2\n");
+	hid_info(hdev, "creating port 2\n");
 	sa->port_two = sanwa_allocate_input_dev(hdev, "Port 2");
 	sanwa_set_capabilities(sa->port_two);
 	ret = input_register_device(sa->port_two);
 	if (ret)
 		return ret;
-	
+
 	return 0;
 }
 
@@ -140,25 +142,29 @@ static int sanwa_probe(struct hid_device *hdev, const struct hid_device_id *id)
 	int ret;
 
 	ret = hid_parse(hdev);
-	if (ret) {
+	if (ret)
+	{
 		hid_err(hdev, "parse failed\n");
 		return ret;
 	}
 
 	ret = hid_hw_start(hdev, HID_CONNECT_HIDRAW);
-	if (ret) {
+	if (ret)
+	{
 		hid_err(hdev, "hw start failed\n");
 		return ret;
 	}
 
 	ret = hid_hw_open(hdev);
-	if (ret) {
+	if (ret)
+	{
 		hid_err(hdev, "hw open failed\n");
 		goto err_stop;
 	}
 
 	ret = sanwa_create_inputs(hdev);
-	if (ret) {
+	if (ret)
+	{
 		hid_err(hdev, "failed to create inputs\n");
 		goto err_close;
 	}
@@ -178,51 +184,39 @@ static int sanwa_raw_event(struct hid_device *hdev, struct hid_report *report, u
 	struct sanwa_input_report *sa_report = (struct sanwa_input_report *)&data[1];
 	struct input_dev *gamepad;
 	uint8_t value;
-	
+
 	if (report->id == 1)
 		gamepad = sa->port_one;
 	else if (report->id == 2)
 		gamepad = sa->port_two;
 
-	if (gamepad) {
-		input_report_abs(gamepad, ABS_X,  sa_report->x);
-		input_report_abs(gamepad, ABS_Y,  sa_report->y);
-		input_report_abs(gamepad, ABS_RX,  sa_report->rx);
+	if (gamepad)
+	{
+		input_report_abs(gamepad, ABS_X, sa_report->x);
+		input_report_abs(gamepad, ABS_Y, sa_report->y);
+		input_report_abs(gamepad, ABS_RX, sa_report->rx);
 		input_report_abs(gamepad, ABS_RY, sa_report->ry);
-		
+
 		value = sa_report->buttons[0] & BUTTONS0_HAT_SWITCH;
 		if (value >= ARRAY_SIZE(sanwa_gamepad_hat_mapping))
 			value = 8;
 		input_report_abs(gamepad, ABS_HAT0X, sanwa_gamepad_hat_mapping[value].x);
 		input_report_abs(gamepad, ABS_HAT0Y, sanwa_gamepad_hat_mapping[value].y);
 
-		// *************************
-		//  BTN_WEST and BTN_NORTH may appear swapped in some applications due to confusion with the button codes.
-		//  In input-event-codes.h they appear as the layout for Nintendo controllers. 
-		//  Strangely, the A and B buttons appear on the xbox layout.
-		// 
-		//  #define BTN_NORTH		0x133
-		//	#define BTN_X			BTN_NORTH
-		//	#define BTN_WEST		0x134
-		//	#define BTN_Y			BTN_WEST
-		//
-		//  I decide to use them swapped since in most cases they are treated as the xbox layout.
-		input_report_key(gamepad, BTN_X,   	  sa_report->buttons[0] & BUTTONS0_SQUARE);
-		input_report_key(gamepad, BTN_Y,  	  sa_report->buttons[0] & BUTTONS0_TRIANGLE);
-		// *************************
+		input_report_key(gamepad, BTN_X, sa_report->buttons[0] & BUTTONS0_SQUARE);
+		input_report_key(gamepad, BTN_Y, sa_report->buttons[0] & BUTTONS0_TRIANGLE);
+		input_report_key(gamepad, BTN_A, sa_report->buttons[0] & BUTTONS0_CROSS);
+		input_report_key(gamepad, BTN_B, sa_report->buttons[0] & BUTTONS0_CIRCLE);
 
-		input_report_key(gamepad, BTN_A,	  sa_report->buttons[0] & BUTTONS0_CROSS);
-		input_report_key(gamepad, BTN_B,	  sa_report->buttons[0] & BUTTONS0_CIRCLE);
-		
-		input_report_key(gamepad, BTN_TL,     sa_report->buttons[1] & BUTTONS1_L1);
-		input_report_key(gamepad, BTN_TR,     sa_report->buttons[1] & BUTTONS1_R1);
-		input_report_key(gamepad, BTN_TL2,    sa_report->buttons[1] & BUTTONS1_L2);
-		input_report_key(gamepad, BTN_TR2,    sa_report->buttons[1] & BUTTONS1_R2);
+		input_report_key(gamepad, BTN_TL, sa_report->buttons[1] & BUTTONS1_L1);
+		input_report_key(gamepad, BTN_TR, sa_report->buttons[1] & BUTTONS1_R1);
+		input_report_key(gamepad, BTN_TL2, sa_report->buttons[1] & BUTTONS1_L2);
+		input_report_key(gamepad, BTN_TR2, sa_report->buttons[1] & BUTTONS1_R2);
 		input_report_key(gamepad, BTN_THUMBL, sa_report->buttons[1] & BUTTONS1_L3);
-		input_report_key(gamepad, BTN_THUMBR, sa_report->buttons[1] & BUTTONS1_R3);	
+		input_report_key(gamepad, BTN_THUMBR, sa_report->buttons[1] & BUTTONS1_R3);
 		input_report_key(gamepad, BTN_SELECT, sa_report->buttons[1] & BUTTONS1_SELECT);
-		input_report_key(gamepad, BTN_START,  sa_report->buttons[1] & BUTTONS1_START);
-	
+		input_report_key(gamepad, BTN_START, sa_report->buttons[1] & BUTTONS1_START);
+
 		input_sync(gamepad);
 	}
 
